cc31e75fe050f22f1f4ed925e516c790688b321f
Franz Buse <franz.buse@tu-ilmenau.de>
2018-06-20 18:37:31 +0200

    first commit

---
+++ src/UuidGenerator.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+
+public class UuidGenerator {
+
+    public static void main(String[] args) {
+        UUID testid = UUID.randomUUID();
+        String convert = testid.toString();
+        System.out.print(convert);
+    }
+
+}
